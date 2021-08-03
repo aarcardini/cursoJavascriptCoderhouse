a481d2dd46c2c013b80f63340d7600ca749f60e2
aarcardini <alejandro.arcardini@hotmail.com>
2021-08-03 20:20:58 -0300

    agregado ejercicio 3

---
+++ clase3/clase3-1.js
@@ -0,0 +1,16 @@
+/* let entrada = prompt("Ingrese su genero");
+while(entrada != "Marciano"){
+   switch (entrada) {
+       case "Hombre" || "hombre":
+            alert("No eres un marciano");
+            break;
+        case "Mujer" || "mujer":
+            alert("No eres una marciana");
+            break;
+       default:
+           alert("¿No eres un Marciano?")
+           break;
+   }
+   entrada = prompt("Ingrese su genero");
+}
+ */
--- clase3/clase3.js
+++ clase3/clase3.js
@@ -1,36 +1,13 @@
 const inputNum = document.getElementById("numero")
-const tablaMulti = document.getElementById("tabla-multi")
+const tablaMulti = document.getElementById("tabla")
 
-function multi() {
+function multiplicar() {
   const numero = parseInt(inputNum.value)
   
   let template = ''
 
   for (let i = 1; i <= 10; i++) {
-      template += numero + "x" + i + "=" + numero * i + "</br>" + "</br>"
+      template += numero + "x" + i + "=" + numero * i  + "</br>"
   }
   tablaMulti.innerHTML = template
 };
-
-
-const inmName = document.getElementById("nombre")
-
-function nombre() {
-
-    let entrada = prompt("Ingresar un nombre");
-    //Repetimos hasta que se ingresa "ESC"
-    while(entrada != "ESC" ){
-    switch (entrada) {
-        case "ANA":
-                alert("HOLA ANA");
-                break;
-            case "JUAN":
-                alert("HOLA JUAN");
-                break;
-        default:
-            alert("¿QUIÉN SOS?")
-            break;
-    }
-    entrada = prompt("Ingresar un nombre");
-    }
-}
