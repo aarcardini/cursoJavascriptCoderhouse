27338594c9bcd492dd52c33104b9ab60dda9e453
aarcardini <alejandro.arcardini@hotmail.com>
2021-08-05 00:15:23 -0300

    se agrega ejercicio 5 falta 4

--- clase5/clase5.js
+++ clase5/clase5.js
@@ -1,20 +1,28 @@
-class GeneradorDeUsuarios {
-    constructor(pNombre, pApellido, pExistente, pPais, pCiudad, pEdad){
-        this.nombre = pNombre;
-        this.apellido = pApellido;
-        this.existente = pExistente;
-        this.pais = pPais;
-        this.ciudad = pCiudad;
-        this.edad = pEdad;
+class Proyecto {
+    constructor(pPromotor, pNombreProyecto, pUbicacion, pRequisitos, pTipo, pScoring, pFinanciado, pConstruccion, pVenta) {
+        this.promotor = pPromotor;
+        this.nombreProyecto = pNombreProyecto;
+        this.ubicacion = pUbicacion;
+        this.requisitos = pRequisitos;
+        this.tipo = pTipo;
+        this.scoring = pScoring;
+        this.financiado = pFinanciado;
+        this.construccion = pConstruccion;
+        this.venta = pVenta;
     }
-
-    seMueve() {
-        console.log("El usuario se movió");
-    };
-    salto() {
-        console.log("El usuario saltó")
+    rentabilidad() {
+        this.rentabilidad = ((this.venta - this.construccion)/this.venta)*100;
     }
-
+    vender() {
+        this.vendido = true;
+    }
 }
 
-var nuevoUsuario = new GeneradorDeUsuarios( "Alejandro", "Arcardini", true, "Uruguay", "Montevideo", 33)
+const proyecto1 = new Proyecto("arar,uy", "Residencia X", "Montevideo", "100000U$S", "Residencia", "A++", "20%", 100000, 140000);
+const proyecto2 = new Proyecto("arar,uy", "Residencia Anne", "Miami", "80000$S", "Residencia", "B+", "20%", 80000, 120000);
+
+proyecto1.rentabilidad();
+proyecto2.rentabilidad();
+
+console.log(proyecto1.rentabilidad);
+console.log(proyecto2.rentabilidad);
